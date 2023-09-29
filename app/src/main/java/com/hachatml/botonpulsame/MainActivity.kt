ba65f92c5a5d364af6778037fc5b6224ca5e8b49
Javier Jiménez-Alfaro Hacha <javilin7967@gmail.com>
2023-09-29 14:37:50 +0200

    comentarios

--- app/src/main/java/com/hachatml/botonpulsame/MainActivity.kt
+++ app/src/main/java/com/hachatml/botonpulsame/MainActivity.kt
@@ -14,27 +14,28 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+    //aquí inicializo la variable para tener el contador globalmente y no dependiente de la función
 var contarBoton = 0
     @SuppressLint("SetTextI18n")
     fun sumarUno(view: View) {
-        contarBoton++
-        var textView = findViewById<TextView>(R.id.textopulsa)
+        contarBoton++ //esta var lleva la cuenta del número de pulsaciones
+        var textView = findViewById<TextView>(R.id.textopulsa) //en estas líneas sincronizamos var con elemento
         var boton = findViewById<Button>(R.id.boton)
-        when (contarBoton){
+        when (contarBoton){ //aquí lo más conveniente es usar un when ya que son los únicos dos que son independientes
             1 -> textView.text = "Has pulsado el botón una vez!"
             2 -> textView.text = "Has pulsado el botón dos veces!"
         }
-        if (contarBoton>2){
+        if (contarBoton>2){ //a partir de aquí con la variable y un condicional nos bastará
             textView.text = "Has pulsado el botón " + contarBoton + " veces!"
         }
         if (contarBoton in 6..9){
-            textView.textSize = 22F
-            textView.text ="Has hecho click varias veces (" + contarBoton + ")!"
+            textView.textSize = 22F //cambio de tamaño
+            textView.text ="Has hecho click varias veces (" + contarBoton + ")!" //y lo mismo que en la 28-29
         }
         if (contarBoton>10){
-            boton.isEnabled = false
-            textView.text = "Te has pasado de clicks, máquina!"
-            Toast.makeText(this, "Se ha desactivado el botón", Toast.LENGTH_SHORT).show()
+            boton.isEnabled = false //desactivamos el botón
+            textView.text = "Te has pasado de clicks, máquina!" //cambiamos el textView
+            Toast.makeText(this, "Se ha desactivado el botón", Toast.LENGTH_SHORT).show() //muestra el mensaje flotante (Toast)
         }
     }
 
